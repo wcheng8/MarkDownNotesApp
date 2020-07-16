4117b2855d9ce56af83fdb7001aaa915c64a2e20
Wilkins Cheng <wilkinscheng@MacBook-Pro.hub>
2020-07-16 14:01:03 +1000

    added Reactrouter

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./App.css";
 import { Container } from "reactstrap";
 import AppNavbar from "./components/AppNavbar";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<AppNavbar />
-			<Container className="pt-4">
-				<Dashboard />
-			</Container>
+			<Router>
+				<AppNavbar />
+				<Container className="pt-4">
+					<Dashboard />
+				</Container>
+			</Router>
 		</div>
 	);
 }
