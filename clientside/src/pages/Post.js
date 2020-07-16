dd9609c79bc65608fcd04be8062a0308f61932f2
Wilkins Cheng <wilkinscheng@MacBook-Pro.hub>
2020-07-16 18:21:11 +1000

    added form to create post

--- clientside/src/pages/Post.js
+++ clientside/src/pages/Post.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 const Post = () => {
 	return (
 		<div>
-			<h1>Posting form here!</h1>
+			<Form>
+				<FormGroup>
+					<Label for="posttitle">Post Title</Label>
+					<Input
+						type="text"
+						name="posttitle"
+						id="posttitle"
+						placeholder="Title of Post"
+					/>
+				</FormGroup>
+				<FormGroup>
+					<Label for="blogtext"></Label>
+					<Input
+						type="textarea"
+						style={{ height: 700 }}
+						name="blog"
+						id="blogtext"
+					/>
+				</FormGroup>
+				<Button>Post</Button>
+			</Form>
 		</div>
 	);
 };
