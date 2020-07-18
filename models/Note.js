3b77b8c6a7930ea08f25ffb9d9fd9da81e7887f2
Wilkins Cheng <wilkinscheng@MacBook-Pro.hub>
2020-07-18 16:24:27 +1000

    added api to deal with posting notes

---
+++ models/Note.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const NoteSchema = mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
+	},
+	// Adds createdAt and updatedAt field
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("Note", NoteSchema);
